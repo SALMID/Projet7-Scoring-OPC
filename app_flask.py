997515024d6ba69233ef0cab42660a71b8011648
SALMID <95616825+SALMID@users.noreply.github.com>
2022-01-21 12:36:47 +0100

    Update app_flask.py

--- app_flask.py
+++ app_flask.py
@@ -1,71 +1,63 @@
-from flask import Flask, jsonify,request
+#APP FLASK (commande : flask run)
+# Partie formulaire non utilisée (uniquement appel à l'API)
+
+from flask import Flask, jsonify
 import pandas as pd
 import pickle
-import numpy as np
-import pandas as pd 
-from lightgbm import LGBMClassifier
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
-
-
 def load_data():
-    
-    #df=pd.read_csv('train_ech.csv',index_col='SK_ID_CURR',encoding ='utf-8')
-    df=pd.read_feather('train_ech')
-    df.index=df['SK_ID_CURR']
-    X=df.copy()
-    X.drop(columns=['TARGET'],inplace=True)    
-    return df,X
+    '''loading the train and original data'''
+    data = pd.read_feather('train_ech')
+    data.index=data['SK_ID_CURR']
+    target = data['TARGET']
+    return data, target
 
 def load_model():
-    #lgbm = joblib.load("model_fin.joblib")
-    
-    pickle_in = open('model_lgbm.pkl', 'rb')
-    lgbm = pickle.load(pickle_in)
-    return lgbm
+    '''loading the trained model'''
+    pickle_in = open('model/model_lgbm.pkl', 'rb')
+    clf_x = pickle.load(pickle_in)
+    return clf_x
 
-def load_prediction(df,id, model):  
-    
-    score_client=model.predict_proba(df[df.index == int(id)])[:,1]        
-    return  score_client
+def load_prediction(sample, id, clf):
+    '''Predict the default probability for a client using SK_CURR_ID'''
+    # X=sample.iloc[:, :-1]
+    X=df.copy()
+    score = clf.predict_proba(X[X.index == int(id)])[:,1]
+    return score
 
 # App config.
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
-#app.config['SECRET_KEY'] = 'NOSECRETKEY'
+app.config['SECRET_KEY'] = 'nosecretkey'
 
-df,X= load_data()
-model= load_model()
-ids_clients = df.index.values
-
+data, sample, target, description = load_data()
+clf = load_model()
+# clf_str_seq = json.dumps(clf)
+ids_clients = data.index.values
 
 @app.route('/')
 def index():
-    return 'test app_flask'
+    return 'app flask !!'
 
-@app.route('/credit/<id_client>', methods=['GET'])
-def credit(id_client):
+@app.route('/credit/<chk_id>', methods=['GET'])
+def credit(chk_id):
     #récupération id client depuis argument url
-    #id_client = request.args.get('id_client', default=1, type=int)
+    #chk_id = request.args.get('chk_id', default=1, type=int)
     #calculer prédiction de la probabilité de défaut
-    prediction = load_prediction(X, id_client, model)
-    
-    # renvoyer la prediction 
+    prediction = load_prediction(X, chk_id, clf)
+    # renvoyer la prediction au demandeur
     dict_final = {
-        'proba' : prediction[0]
+        'proba' : float(prediction)
         }
     return jsonify(dict_final)
-
-    
 
 #lancement de l'application
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
 
 
